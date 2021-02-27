8711d8d32257f37f608e7b07dcb513b7b263dcd3
jasingh784 <48878742+jasingh784@users.noreply.github.com>
2021-02-26 20:32:32 -0800

    changed port variable to check for port in process.env first

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 
 mongoose.connect(process.env.ATLAS_URI, {
